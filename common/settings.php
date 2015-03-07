0275314bdc1d3abf562c31724f8aa8a3288597fe
Muhammad Hanif <thehanif@msn.com>
2015-03-07 18:40:36 +0500

    Site Functionality
    Define which feature to enabled from setting file.

---
+++ common/settings.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Site functionality setting's variables
+ */
+
+// Blog
+define('HAS_BLOG', true);					// Manage blog
+
+// Catelog
+define('HAS_CATALOG', true);				// Able to manage products
+define('HAS_CATALOG_PAYMENT', true);		// Add payment module and checkout options for products
+define('HAS_CATALOG_SHIPPING', true);		// Add shipping modules for products
+define('HAS_CATALOG_MANUFACTURERS', true);	// Add multiple manufacturer options
+
+// Users
+define('HAS_USERS', true); 					// Able to manage users
+define('HAS_USERS_ROLE', true); 			// Able to manage user's role
+
+// Plugins
+define('HAS_PLUGINS', true);				// Manage Plugins
+
+// Settings
+define('HAS_PERFORMANCE', true);			// Manage site performance and cache system
+define('HAS_BACKUP', true);					// Create and manage site backup
+define('HAS_LANGUAGE', true);				// Manage admin language
