c5170bcd0eb40c5239ad52dd6c61e3ea8e284709
Muhammad Hanif <thehanif@msn.com>
2015-03-07 18:22:23 +0500

    DB Config
    Adding database configuration file to root directory.

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Please enter your database details
+ */
+
+// Database Server path
+define('DB_HOST', 'localhost');
+
+// Database name
+define('DB_NAME', 'newapp');
+
+// Database Username
+define('DB_USER', 'root');
+
+// Database Password
+define('DB_PASS', 'root');
+
+
+/**
+ * Warning! Don't need to edit below
+ */
+
+// Absolute path to the root directory
+if ( !defined('ABSPATH') )
+define('ABSPATH', dirname(__FILE__) . "/");
