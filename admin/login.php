f7eb5c9ea4401224c53c2596dc17422c9629c36f
Muhammad Hanif <thehanif@msn.com>
2015-03-08 21:26:08 +0500

    Login
    Added redirection and login status,
    Added errors html,
    Added dynamic action url

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,25 @@
 <?php 
 // Initialization
 include_once 'include/init.php';
+
+if (is_logged_in(false)) {
+
+  // Redirection previous URL
+  if (isset($_GET['URL'])) {
+    header('location:'.urldecode($_GET['URL']));
+    exit;
+  }
+
+  // Redirect to dashboard
+  header('location:index.php');
+}
+
+$user = new users();
+
+if (isset($_POST['submit'])) {
+  print_f($_POST);
+}
+
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
@@ -18,23 +37,23 @@ include_once 'include/init.php';
   <section class="container">
     <div class="login">
       <h1><?php echo _('Login'); ?></h1>
-      <form method="post" action="index.html">
-        <p><input type="text" name="login" value="" placeholder="<?php echo _('Username or Email'); ?>"></p>
+      <form method="post" action="login.php<?php echo (isset($_GET['URL']))? '?url='.urlencode($_GET['URL']) : ''; ?>">
+        <p><input type="text" name="login" value="" placeholder="<?php echo _('Username'); ?>"></p>
         <p><input type="password" name="password" value="" placeholder="<?php echo _('Password'); ?>"></p>
+        
+        <p class="login-help error"><?php echo (isset($error_login))? _($error_login) : ''; ?></p>
         <!-- <p class="remember_me">
           <label>
             <input type="checkbox" name="remember_me" id="remember_me">
             Remember me on this computer
           </label>
         </p> -->
-        <p class="submit"><input type="submit" name="commit" value="<?php echo _('Login'); ?>"></p>
+        <p class="submit"><input type="submit" name="submit" value="<?php echo _('Login'); ?>"></p>
       </form>
     </div>
-
     <div class="login-help">
       <p><?php echo _('Forgot your password?'); ?> <a href="index.html"><?php echo _('Click here to reset it.'); ?></a></p>
     </div>
   </section>
-
 </body>
 </html>
