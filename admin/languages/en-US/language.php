d1f6094a0c55ba6c4d6e4bcf4f369dd25bca1314
Muhammad Hanif <thehanif@msn.com>
2015-03-08 16:15:53 +0500

    Login
    Added login texts

--- admin/languages/en-US/language.php
+++ admin/languages/en-US/language.php
@@ -6,3 +6,9 @@
 
 $language['Title'] = 'Title';
 $language['Dashboard'] = 'Dashboard';
+$language['Login Form'] = 'Login Form';
+$language['Login'] = 'Login';
+$language['Click here to reset it.'] = 'Click here to reset it.';
+$language['Forgot your password?'] = 'Forgot your password?';
+$language['Password'] = 'Password';
+$language['Username or Email'] = 'Username or Email';
