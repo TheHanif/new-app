bbcff86267ced4ea4e0afef55438b76ca123921d
Muhammad Hanif <thehanif@msn.com>
2015-03-09 14:08:04 +0500

    Users
    Added login method,
    Added logout method,
    Added get user profile method

--- admin/classes/class.users.php
+++ admin/classes/class.users.php
@@ -1,9 +1,14 @@
 <?php
 class Users extends Database{
 
+	private $table_name;
+	private $profile_table_name;
+
 	public function __construct()
 	{
 		parent::__construct();
+		$this->table_name = DB_PREFIX.'users';
+		$this->profile_table_name = DB_PREFIX.'user_profiles';
 	}
 
 	// Get login status
@@ -15,4 +20,76 @@ class Users extends Database{
 			return false;
 		}
 	}
-}
+
+	/**
+	 * Login user
+	 * @param  array $info submited form data
+	 * @return boolean
+	 */
+	public function do_login($info)
+	{	
+		// Filter password
+		$password = md5($info['password']);
+
+		// Prepare where statement
+		$this->where('user_username', $info['login']);
+		$this->where('user_password', $password);
+
+		// Select user primary key
+		$this->select(array('user_id'=>'user_id'));
+
+		// From table
+		$this->from($this->table_name);
+
+		// If provided info is correct, login user
+		if ($this->row_count() > 0) {
+			
+			$results = $this->result();
+
+			$_SESSION['token'] = time();
+			$_SESSION['user_id'] = $results->user_id;
+
+			return true;
+		}else{
+			return false;
+		}
+	} // end of do_login()
+
+	/**
+	 * Logout user remove session
+	 * @return boolean
+	 */
+	public static function do_logout()
+	{	
+		if (isset($_SESSION['token'])) {
+			unset($_SESSION['user_id']);
+			unset($_SESSION['token']);
+			return true;
+		}
+	} // end of do_logout()
+
+	/**
+	 * Get primary key of logged user
+	 * @return int
+	 */
+	public static function get_logged_id()
+	{
+		if (isset($_SESSION['user_id'])) {
+			return $_SESSION['user_id'];
+		}
+	} // end of get_logged_id()
+
+	/**
+	 * Get user profile detail
+	 * @param  int $user_id
+	 * @return object
+	 */
+	public function get_profile($user_id)
+	{
+		$this->where('user_id', $user_id);
+		$this->from($this->profile_table_name);
+		if ($this->row_count() > 0) {
+			return $this->result();
+		}
+	} // end of get_profile()
+} // end of class
