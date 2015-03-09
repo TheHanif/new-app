2d5d13e6c3ee638c20c73f97fa02562e99a24fed
Muhammad Hanif <thehanif@msn.com>
2015-03-09 22:00:06 +0500

    Builtin Functionalities
    Created default functionalities file

---
+++ admin/builtin/navigations.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Admin sidebar navigation
+ */
+$dashboard = array(
+		'title' => 'Dashboard'
+		,'icon' => 'dashboard'
+		,'file' => 'index.php'
+	);
+$admin_sidebar_navigation['dashboard'] = $dashboard;
