ebb94eb7ddf4cc731904d0c72595841e18066d97
Muhammad Hanif <thehanif@msn.com>
2015-03-09 14:09:28 +0500

    SQL Updated
    Added tables
    user_profile
    user_role

--- sql_data/new_app.sql
+++ sql_data/new_app.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.9
+-- version 4.0.6
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 08, 2015 at 10:31 AM
--- Server version: 5.5.20
--- PHP Version: 5.3.9
+-- Generation Time: Mar 09, 2015 at 02:08 PM
+-- Server version: 5.5.33
+-- PHP Version: 5.3.27
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `new_app`
@@ -26,13 +20,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `settings`
 --
 
-CREATE TABLE IF NOT EXISTS `settings` (
+CREATE TABLE `settings` (
   `setting_id` int(11) NOT NULL AUTO_INCREMENT,
   `setting_name` varchar(32) NOT NULL,
   `setting_value` longtext NOT NULL,
   `setting_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`setting_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `settings`
@@ -41,6 +35,62 @@ CREATE TABLE IF NOT EXISTS `settings` (
 INSERT INTO `settings` (`setting_id`, `setting_name`, `setting_value`, `setting_ts`) VALUES
 (1, 'language', 'en-US', '2015-03-08 08:07:49');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_username` varchar(32) NOT NULL,
+  `user_password` varchar(256) NOT NULL,
+  `user_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_username`, `user_password`, `user_ts`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2015-03-09 05:31:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_profiles`
+--
+
+CREATE TABLE `user_profiles` (
+  `user_profile_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(128) NOT NULL,
+  `user_display_name` varchar(128) NOT NULL,
+  `user_email` varchar(128) NOT NULL,
+  `user_image` varchar(128) NOT NULL,
+  `user_role` int(11) NOT NULL,
+  PRIMARY KEY (`user_profile_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `user_profiles`
+--
+
+INSERT INTO `user_profiles` (`user_profile_id`, `user_id`, `user_name`, `user_display_name`, `user_email`, `user_image`, `user_role`) VALUES
+(1, 1, 'Muhammad Hanif', 'The Hanif', 'thehanif@msn.com', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_title` varchar(32) NOT NULL,
+  `role_description` varchar(250) NOT NULL,
+  `role_object` longtext NOT NULL,
+  `role_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
