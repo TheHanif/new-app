6449cd2496c38208719f6b16dd2c6a3a622151a5
Muhammad Hanif <thehanif@msn.com>
2015-03-10 17:28:04 +0500

    cleaning
    cleaning

--- admin/builtins/navigations.php
+++ admin/builtins/navigations.php
@@ -8,6 +8,8 @@ add_navigation_item('dashboard', 'Dashboard', 'dashboard', 'index.php');
 
 // Users
 add_navigation_item('users', 'Users', 'users', '#');
+
+// User submenu
 add_navigation_item('users', 'Users', 'users', 'users.php', 'users');
 add_navigation_item('create_user', 'Create user', 'plus', 'create_user.php', 'users');
 add_navigation_item('user_role', 'User roles', 'check', 'user_roles.php', 'users');
