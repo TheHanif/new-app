5819691ccf8bd63f246ff7be90d203e8ef27a53a
Muhammad Hanif <thehanif@msn.com>
2015-03-10 14:53:01 +0500

    Users
    Init Create user for sidebar testing of submenu item active class

---
+++ admin/create_user.php
@@ -0,0 +1,40 @@
+<?php 
+// Initialization
+include_once 'include/init.php';
+
+// Page title
+$admin_title = 'Create User';
+
+// Variable to active select page
+// $page = get_script_name();
+
+// Header file
+include 'include/header.php';
+?>
+<!-- BEGIN MAIN PAGE CONTENT -->
+<div id="page-wrapper">
+	<!-- BEGIN PAGE HEADING ROW -->
+		<div class="row">
+			<div class="col-lg-12">
+				<!-- BEGIN BREADCRUMB -->
+				<?php include 'include/breadcrumb.php'; ?>
+				<!-- END BREADCRUMB -->	
+				
+				<div class="page-header title">
+				<!-- PAGE TITLE ROW -->
+					<h1><?php echo __($admin_title); ?> <span class="sub-title">sub title</span></h1>								
+				</div>
+				
+			</div><!-- /.col-lg-12 -->
+		</div><!-- /.row -->
+	<!-- END PAGE HEADING ROW -->					
+		<div class="row">
+			<div class="col-lg-12">
+			
+			<!-- START YOUR CONTENT HERE -->
+				<p>This is a light-weight blank page, with minimum to none plugins loaded</p>
+			<!-- END YOUR CONTENT HERE -->
+	
+			</div>
+		</div>
+<?php include 'include/footer.php'; ?>
