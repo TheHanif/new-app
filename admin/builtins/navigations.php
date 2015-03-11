912e5f9e19508b1e4af7b42c7add55503640e17f
Muhammad Hanif <thehanif@msn.com>
2015-03-11 16:37:08 +0500

    Fixes
    Fixes

--- admin/builtins/navigations.php
+++ admin/builtins/navigations.php
@@ -10,9 +10,9 @@ add_navigation_item('dashboard', 'Dashboard', 'dashboard', 'index.php');
 add_navigation_item('users', 'Users', 'users', '#', NULL, HAS_USERS);
 
 // User submenu
-add_navigation_item('users', 'Users', 'users', 'users.php', 'users', NULL, HAS_USERS);
-add_navigation_item('create_user', 'Create user', 'plus', 'create_user.php', 'users', NULL, HAS_USERS);
-add_navigation_item('user_role', 'User roles', 'check', 'user_roles.php', 'users', NULL, HAS_USERS);
+add_navigation_item('users', 'Users', 'users', 'users.php', 'users');
+add_navigation_item('create_user', 'Create user', 'plus', 'create_user.php', 'users');
+add_navigation_item('user_role', 'User roles', 'check', 'user_roles.php', 'users', HAS_USERS_ROLE);
 
 
 // Settings
