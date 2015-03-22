2304420b4faec7bf0c66e52191eda4cdf51ceee7
Muhammad Hanif <thehanif@msn.com>
2015-03-22 14:39:52 +0500

    Language
    added for testing settings

---
+++ admin/languages/ur-PK/language.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Name: Urdu
+ * Country: Pakistan
+ * Direction : RTL
+ */
+
+$language['Title'] = 'Title';
+$language['Dashboard'] = 'Dashboard';
+$language['Login Form'] = 'Login Form';
+
+$language['Incorrect Username or Password, please try again.'] = 'Incorrect Username or Password, please try again.';
+$language['Click here to reset it.'] = 'Click here to reset it.';
+$language['Forgot your password?'] = 'Forgot your password?';
+$language['Password'] = 'Password';
+$language['Username'] = 'Username';
+$language['Settings'] = 'Kuj nahe';
