d950528a0c538142778358b36b99daaa3bc5eeff
Muhammad Hanif <thehanif@msn.com>
2015-03-27 00:24:28 +0500

    Cleaning & custom post
    Separated procedural functions for front and back
    Added custom post registe

--- admin/builtins/navigations.php
+++ admin/builtins/navigations.php
@@ -3,8 +3,13 @@
  * Admin sidebar navigation
  */
 
+//add_navigation_item($name, $title, $icon, $file, $child_files = array(), $parent = NULL, $settings = NULL, $capability = NULL)
+
 // Dashboard
 add_navigation_item('dashboard', 'Dashboard', 'dashboard', 'index.php');
+
+// Blog
+// add_navigation_item('blog', 'Blog', 'thumb-tack', '#', array(), NULL, HAS_BLOG, array('Blog'=>array('manage-blog')));
 
 // Media
 add_navigation_item('media', 'Media', 'camera', 'media.php', array('media_upload.php', 'media_edit.php'));
@@ -17,8 +22,5 @@ add_navigation_item('users', 'Users', 'users', 'users.php', array(), 'users', NU
 add_navigation_item('create_user', 'Create user', 'plus', 'create_user.php', array(), 'users', NULL, array('Users'=>array('create-users')));
 add_navigation_item('user_role', 'User roles', 'check', 'user_roles.php', array('create_role.php'), 'users', HAS_USERS_ROLE, array('Users'=>array('manage-roles')));
 
-
 // Settings
 add_navigation_item('settings', 'Settings', 'cogs', 'settings.php', array(), NULL, NULL, array('Settings'=>array('manage-settings')));
-
-//add_navigation_item($name, $title, $icon, $file, $child_files = array(), $parent = NULL, $settings = NULL, $capability = NULL)
--- common/procedural_functions.php
+++ common/procedural_functions.php
@@ -1,337 +1,46 @@
 <?php
-// Custom auto loader
-function class_autoloader($class) {
-    include_once ADMINABS.'classes/class.' . $class . '.php';
-}
-
-// Register custom auto loader as > php 5.1.2
-spl_autoload_register('class_autoloader');
-
+/**
+ * Procedural Functiobs for Admin and Front end
+ */
 
 /**
- * Translator function
- * 
- * @param string $key Language key
+ * Register custom post
+ * @param array $args Post features
  */
-$lang = new language();
 
-function __($key, $echo = true)
-{	
-	global $lang;
+// $args = array(
+// 		'id' 		=>	'blog'
+// 		,'title'	=> 'Blog'
+// 		,'meta'		=>	array(
+// 					'title' => 'Post'
+// 					,'single_title' => 'post'
+// 					,'pulural_title' => 'Posts'
+// 					)
+// 		,'cattegory'	=> true
+// 		,'featured_image'=> true
+// 		,'tag'	=> true
+// 		,'template' => true
+// 		,'searchable'=> true
+// 	);
 
-	if ($echo) {
-		echo $lang->get_key($key);
-	}else{
-		return $lang->get_key($key);
-	}
-}
+function register_post($args = array()){
 
-// Alternate of print_r
-function print_f($data, $exit = false)
-{
-	echo '<pre>';
-	print_r($data);
-	echo '</pre>';
-
-	if($exit) exit;
-}
-
-// Get current URL from address bar
-function get_actual_url($encode = true)
-{
-	$actual_url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	if ($encode) {
-		return urlencode($actual_url);
-	}else{
-		return $actual_url;
-	}
-}
-
-/**
- * Check user login status
- * 
- * @param boolean $redirect
- */
-function is_logged_in($redirect = false)
-{
-	$status = Users::is_logged_in();
-
-	// $URL = (isset($_GET['URL']))? urlencode($_GET['URL']) : get_actual_url();
-
-	if (!$status && $redirect) {
-		header("location:login.php?return=".get_actual_url());
-	}
-
-	return $status;
-}
-
-/**
- * Get requested file name
- * @return string
- */
-function get_script_name()
-{
-	return end(explode('/', rtrim($_SERVER['SCRIPT_NAME'], '.php')));
-}
-
-function is_page_active($page, $echo = true)
-{	
-	if ((is_array($page) && in_array(get_script_name(), $page)) || ($page == get_script_name())) {
-		if ($echo) {
-			echo "active";
-		}else{
-			return 'active';
-		}
-	}
-}
-
-/**
- * Generate Admin sidebar navigation $admin_sidebar_navigation
- */
-function generate_admin_menu()
-{
-	global $admin_sidebar_navigation;
-
-	foreach ($admin_sidebar_navigation as $key => $data) {
-		$parent_active = '';
-		if (strpos($data['file'], '.')) {
-			$file = array_shift(array_slice(explode('.', $data['file']), 0, 1));
-
-			$data['child_files'][] = $file;
-			$parent_active = is_page_active($data['child_files'], false);
-
-			// Go to unlock the page
-			unlock_page(is_page_active($data['child_files'], false), $data['has_settings'], $data['has_capability']);
-		}
-	?>
-		<li class="<?php echo (isset($data['submenu']))? check_submenu($data['submenu']) : ''; ?>">
-			<?php if (isset($data['submenu'])) {
-				echo '<a href="javascript:;" data-parent="#side" data-toggle="collapse" class="accordion-toggle" data-target="#'.$key.'">';
-			}else{
-				?><a class="<?php echo $parent_active; ?>" href="<?php echo $data['file']; ?>"><?php
-				} ?>
-				<i class="fa fa-<?php echo $data['icon']; ?>"></i> <?php __($data['title']); ?> <?php echo (isset($data['submenu']))? '<span class="fa arrow"></span>' : ''; ?>
-			</a>
-			<?php // Get Submenu
-				if (isset($data['submenu'])) {
-					?>
-						<ul class="collapse nav" id="<?php echo $key; ?>">
-							<?php // Submenu loop
-								foreach ($data['submenu'] as $submenu_key => $submenu_data) {
-									$submenu_item_active = '';
-									if (strpos($submenu_data['file'], '.')) {
-										$file = array_shift(array_slice(explode('.', $submenu_data['file']), 0, 1));
-
-										$submenu_data['child_files'][] = $file;
-										$submenu_item_active = is_page_active($submenu_data['child_files'], false);
-
-										// Go to unlock the page
-										unlock_page(is_page_active($submenu_data['child_files'], false), $submenu_data['has_settings'], $submenu_data['has_capability']);
-									}
-									?>
-										<li>
-											<a class="<?php echo $submenu_item_active; ?>" href="<?php echo $submenu_data['file']; ?>">
-												<i class="fa fa-<?php echo $submenu_data['icon']; ?>"></i> <?php __($submenu_data['title']); ?>
-											</a>
-										</li>
-									<?php
-								} // end for foreach
-							?>
-						</ul>
-					<?php
-				} // end submenu
-			 ?>
-		</li>
-	<?php
-	} // end foreach
-} // end of generate_admin_menu()
-
-/**
- * Unlock admin page
- * 
- * @return boolean	true if unlocked
- */
-function unlock_page($is_active, $has_settings, $has_capabilities)
-{
-	global $lock_page;
-	if ($is_active && is_allowed($has_settings, $has_capabilities)) {
-		$lock_page = false;
-		return true;
-	}
-} // end of unlock_page()
-
-/**
- * Get the lock status of page
- * If false, include footer file and exit the page to prevent loading of page contents
- */
-function get_lock_status()
-{	
-	global $lock_page;
-
-	if ($lock_page) {
-		generate_message('ACCESS DENIED!', 'You do not have sufficient capabilities to access this area.', 'danger');
-		include ADMINABS.'include/footer.php';
-		exit;
-	}
-} // end of get_lock_status()
-
-/**
- * Get classes for submenus
- */
-function check_submenu($data)
-{
-	$open = '';
-	foreach ($data as $key => $value) {
-		if (strpos($value['file'], '.')) {
-			$file = array_shift(array_slice(explode('.', $value['file']), 0, 1));
-
-			$value['child_files'][] = $file;
-
-			if (is_page_active($value['child_files'], false)) {
-				$open = 'open';
-			}
-		}
-	}// end foreach
-
-	$class = 'panel '.$open;
-	return $class;
-} // check_submenu
-
-/**
- * Add item admin sidebar navigation
- * @param string $name   item key
- * @param string $title  item text
- * @param string $icon   fa icon
- * @param string $file   file name
- * @param string $parent parent key
- */
-function add_navigation_item($name, $title, $icon, $file, $child_files = array(), $parent = NULL, $settings = NULL, $capability = NULL)
-{
-	global $admin_sidebar_navigation;
-
-	// Check rights and capabilities
-	if (!is_allowed($settings, $capability)) {
+	// check if array
+	if (!is_array($args)) {
 		return;
 	}
 
-	// Remove extensions
-	foreach ($child_files as $key => $value) {
-		$child_files[$key] = rtrim($value, '.php');
-	}
+	global $posts;
 
-	// Prepare item data
-	$item = array();
-	$item = array(
-		'title' => $title
-		,'icon' => $icon
-		,'file' => $file
-		,'child_files' => $child_files
-		,'has_settings' => $settings
-		,'has_capability' => $capability
-	);
-
-	if (isset($parent)) { // Submenu item
-		if (isset($admin_sidebar_navigation[$parent])) {
-			$admin_sidebar_navigation[$parent]['submenu'][$name]=$item;
+	// Prevent from overriding
+	if (isset($posts[$args['id']])) {
+		if (function_exists('register_admin_message')) {
+			register_admin_message('Custom post', 'Can not register custom post, post already registered.', 'danger');
 		}
-	}else{ // Top level item
-		$admin_sidebar_navigation[$name]=$item;
+		return;
 	}
 
-} // end of add_navigation_item
+	// Add post
+	$posts[$args['id']] = $args;
 
-/**
- * Add message for admin area
- * @param  string $title   Message title
- * @param  string $message Message
- * @param  string $type    alert type
- */
-function register_admin_message($title, $message, $type)
-{
-	global $messages;
-
-	$message = array(
-			'title' => $title
-			,'message' => $message
-			,'type' => $type
-		);
-	$messages[] = $message;
-} // register_admin_message
-
-/**
- * Get all messages for admin
- * @return string alert HTML markup
- */
-function get_messages()
-{
-	global $messages;
-	$types = array('warning', 'info', 'danger', 'success', 'primary', 'inverse');
-
-	foreach ($messages as $message) {
-		if(!in_array($message['type'], $types)) continue;
-		?>
-		<div class="alert bg-<?php echo $message['type'] ?>">
-			<button aria-hidden="true" data-dismiss="alert" class="close" type="button">x</button>
-			<p><strong><?php __($message['title']); ?>:</strong><br>  <?php __($message['message']); ?></p>
-		</div>
-		<?php
-	} // end of foreach
-} // end of get_messages()
-
-/**
- * Generate alert message for admin
- * @return string alert HTML markup
- */
-function generate_message($title, $message, $type)
-{
-	$types = array('warning', 'info', 'danger', 'success', 'primary', 'inverse');
-
-	if(!in_array($type, $types)) return;
-	?>
-	<div class="alert bg-<?php echo $type; ?>">
-		<button aria-hidden="true" data-dismiss="alert" class="close" type="button">x</button>
-		<p><strong><?php __($title); ?>:</strong><br>  <?php __($message); ?></p>
-	</div>
-	<?php
-} // end of get_messages()
-
-/**
- * Check if user have capabities in some areas
- * @param  contants  $settings   main settings variables
- * @param  array  $capability user role have rights in this
- * @return boolean
- */
-function is_allowed($settings = null, $capability = null){
-
-	global $Users;
-
-	// By default every thing is allowed.
-	$state = true;
-
-	// Default settings
-	if ((isset($settings) && $settings == false)) {
-		$state = false;
-	}
-
-	// User Rights and capabilitirs
-	if ((isset($capability) && !$Users->has_capabilities($capability))) {
-		$state = false;
-	}
-
-	return $state;
-} // end of is_allowed()
-
-
-/**
- * Add new capabilty
- * @param  string $group
- * @param  string $key
- */
-function register_capability($group, $key)
-{
-	global $capabilities_groups;
-
-	if (isset($capabilities_groups[$group])) {
-		$capabilities_groups[$group][] = $key;
-	}
-} // end of register_capability()
+} // end of register_post()
