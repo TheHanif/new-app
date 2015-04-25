d666e56e23361960ca83d3f288ed8057acaeece4
Muhammad Hanif <thehanif@msn.com>
2015-04-25 15:07:59 +0500

    SQL
    Updated SQL file

--- sql_data/new_app.sql
+++ sql_data/new_app.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 10, 2015 at 09:33 PM
+-- Generation Time: Apr 25, 2015 at 10:07 AM
 -- Server version: 5.5.20
 -- PHP Version: 5.3.9
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `new_app`
@@ -38,23 +32,14 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `category_type` varchar(32) NOT NULL,
   `category_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`category_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`category_id`, `category_name`, `category_slug`, `category_description`, `category_media`, `category_parent`, `category_item_count`, `category_order`, `category_type`, `category_ts`) VALUES
-(12, 'Category 1', 'category-1-2', '', 0, 0, NULL, 0, 'services', '2015-04-03 08:35:03'),
-(26, 'Sample category 4', 'sample-category-4', '', 0, 24, NULL, 0, 'blog', '2015-04-06 18:42:52'),
-(25, 'Sample category 3', 'sample-category-3', '', 0, 0, NULL, 0, 'blog', '2015-04-06 18:42:41'),
-(17, 'ferqr3232', 'ferqr3232', '', 0, 15, NULL, 0, 'blog', '2015-04-03 12:40:06'),
-(24, 'Sample category 2', 'sample-category-2', '', 0, 23, NULL, 0, 'blog', '2015-04-06 18:42:31'),
-(23, 'Sample category 1', 'sample-category-1', '', 0, 0, NULL, 0, 'blog', '2015-04-06 18:42:21'),
-(27, 'Sample category 5', 'sample-category-5', '', 0, 0, NULL, 0, 'blog', '2015-04-06 18:42:59'),
-(28, 'Sample category 6', 'sample-category-6', '', 0, 23, NULL, 0, 'blog', '2015-04-06 18:43:45'),
-(29, 'Sample category 7', 'sample-category-7', '', 0, 0, NULL, 0, 'blog', '2015-04-06 18:44:51'),
-(30, 'Sample category 8', 'sample-category-8', '', 0, 0, NULL, 0, 'blog', '2015-04-06 18:44:56');
+(1, 'Sample category 1', 'sample-category-1', '', 0, 0, 5, 0, 'blog', '2015-04-25 09:38:33');
 
 -- --------------------------------------------------------
 
@@ -65,10 +50,36 @@ INSERT INTO `categories` (`category_id`, `category_name`, `category_slug`, `cate
 CREATE TABLE IF NOT EXISTS `meta` (
   `meta_id` int(11) NOT NULL AUTO_INCREMENT,
   `object_id` int(11) NOT NULL,
-  `key` varchar(100) NOT NULL,
-  `value` longtext NOT NULL,
+  `meta_key` varchar(100) NOT NULL,
+  `meta_value` longtext NOT NULL,
   PRIMARY KEY (`meta_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+
+--
+-- Dumping data for table `meta`
+--
+
+INSERT INTO `meta` (`meta_id`, `object_id`, `meta_key`, `meta_value`) VALUES
+(1, 23, 'category', '1'),
+(2, 23, 'template', '*none'),
+(3, 23, 'sidebar', '*none'),
+(4, 23, 'featured_image', '["","",""]'),
+(5, 24, 'category', '1'),
+(6, 24, 'template', '*none'),
+(7, 24, 'sidebar', '*none'),
+(8, 24, 'featured_image', '["","",""]'),
+(10, 25, 'template', 'default'),
+(11, 25, 'sidebar', 'left'),
+(12, 25, 'featured_image', '["","",""]'),
+(13, 25, 'category', '1'),
+(15, 26, 'template', 'default'),
+(16, 26, 'sidebar', 'left'),
+(17, 26, 'featured_image', '["","",""]'),
+(18, 26, 'category', '1'),
+(19, 27, 'category', '1'),
+(20, 27, 'template', 'default'),
+(21, 27, 'sidebar', 'left'),
+(22, 27, 'featured_image', '["","",""]');
 
 -- --------------------------------------------------------
 
@@ -78,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `meta` (
 
 CREATE TABLE IF NOT EXISTS `objects` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `authod` int(11) NOT NULL,
+  `author` int(11) NOT NULL,
   `title` varchar(256) NOT NULL,
   `content` longtext NOT NULL,
   `excerpt` longtext NOT NULL,
@@ -90,7 +101,18 @@ CREATE TABLE IF NOT EXISTS `objects` (
   `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_ts` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=80 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+
+--
+-- Dumping data for table `objects`
+--
+
+INSERT INTO `objects` (`ID`, `author`, `title`, `content`, `excerpt`, `name`, `parent`, `status`, `mimetype`, `type`, `ts`, `modified_ts`) VALUES
+(27, 1, 'Sample post 5', '', '', 'sample-post-5', 24, 'published', '', 'blog', '2015-04-25 10:00:57', '0000-00-00 00:00:00'),
+(26, 1, 'Sample post 4', '', '', 'sample-post-4', 25, 'published', '', 'blog', '2015-04-25 10:00:39', '0000-00-00 00:00:00'),
+(25, 1, 'Sample post 3', '', '', 'sample-post-3', 24, 'published', '', 'blog', '2015-04-25 10:00:24', '0000-00-00 00:00:00'),
+(24, 1, 'Sample post 2', '', '', 'sample-post-2', 23, 'published', '', 'blog', '2015-04-25 10:00:12', '0000-00-00 00:00:00'),
+(23, 1, 'Sample post 1', 'contents', '', 'sample-post-1', 0, 'published', '', 'blog', '2015-04-25 09:48:45', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -183,9 +205,5 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
 --
 
 INSERT INTO `user_roles` (`role_id`, `role_title`, `role_description`, `role_object`, `role_ts`) VALUES
-(2, 'Administrator', 'This role has all the capabilities.', '{"Site":{"manage-pages":"1","create-pages":"1","publish-pages":"1","edit-pages":"1","delete-pages":"1","manage-themes":"1"},"Catalog":{"manage-catalog":"1","add-products":"1","edit-products":"1","delete-products":"1","manage-reviews":"1","manage-payment-methods":"1","manage-shipping-methods":"1","manage-orders":"1","edit-orders":"1","delete-orders":"1","manage-customers":"1"},"Users":{"manage-users":"1","create-users":"1","delete-users":"1","manage-roles":"1"},"Plugins":{"manage-plugins":"1"},"Settings":{"manage-settings":"1"},"blog":{"manage":"1","create":"1"},"services":{"manage":"1","create":"1"}}', '2015-03-14 16:53:29'),
+(2, 'Administrator', 'This role has all the capabilities.', '{"Site":{"manage-pages":"1","create-pages":"1","publish-pages":"1","edit-pages":"1","delete-pages":"1","manage-themes":"1"},"Catalog":{"manage-catalog":"1","add-products":"1","edit-products":"1","delete-products":"1","manage-reviews":"1","manage-payment-methods":"1","manage-shipping-methods":"1","manage-orders":"1","edit-orders":"1","delete-orders":"1","manage-customers":"1"},"Users":{"manage-users":"1","create-users":"1","delete-users":"1","manage-roles":"1"},"Plugins":{"manage-plugins":"1"},"Settings":{"manage-settings":"1"},"blog":{"manage":"1","create":"1"},"services":{"manage":"1"}}', '2015-03-14 16:53:29'),
 (3, 'Test', '', 'null', '2015-04-03 14:08:05');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
