f5d19681b92218b9f956d82755fde83b4ba35e7f
Muhammad Hanif <thehanif@msn.com>
2015-05-01 11:24:49 +0500

    Post listing
    Added category filter

--- admin/posts.php
+++ admin/posts.php
@@ -42,7 +42,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete') {
 }
 
 // Action
-if (isset($_POST['action'])) {
+if (isset($_POST['submit'])) {
 	foreach ($_POST['posts'] as $ID) {
 		if($_POST['action'] == 'deleted'){
 			$post = $Posts->get_post($ID);
@@ -72,13 +72,24 @@ $fields['status'] = 'status';
 $fields['ts'] = 'cts';
 $fields['modified_ts'] = 'mts';
 
+
+// Filter by category
+if (isset($_POST['filter']) || isset($_GET['category'])) {
+
+	$category = (isset($_POST['category'])) ? $_POST['category'] : $_GET['category'];
+
+	$Posts->inner_join('meta', 'c', 'c.object_id = objects.ID');
+	$Posts->where('c.meta_key', 'category');
+	$Posts->where('c.meta_value', $category);
+} // end filter
+
 if (isset($_GET['status'])) {
 	$Posts->where('status', $_GET['status']);
 }
 
 $Posts->select($fields);
 $all_posts = $Posts->get_posts($type);
-
+// print_f($all_posts, 1);
 
 // Page title
 $admin_title = ucfirst($custom_names);
@@ -112,33 +123,58 @@ include 'include/header.php';
 		get_lock_status(); ?>
 		
 		<!-- START YOUR CONTENT HERE -->
-		<form action="" method="post" onSubmit="return confirm('<?php echo 'Do you realy want to apply this action?' ?>');">
+		<form action="" method="post">
 			<div class="row">
 				<div class="col-lg-12">
 					
 					<div class="well white">
 						<div class="row">
-							<div class="col-sm-6">
+							<div class="col-sm-5">
 								<a href="posts.php?type=<?php echo $type; ?>" class="btn btn-sm <?php echo (!isset($_GET['status']))? 'btn-primary active disabled' : ''; ?>">All <small class="badge <?php echo (!isset($_GET['status']))? 'badge-inverse' : 'badge-primary'; ?>"><?php echo $Posts->get_post_count($type); ?></small></a>
 								<a href="posts.php?type=<?php echo $type; ?>&status=published" class="btn btn-sm <?php echo (isset($_GET['status']) && $_GET['status'] == 'published')? 'btn-primary active disabled' : ''; ?>">Published <small class="badge <?php echo (isset($_GET['status']) && $_GET['status'] == 'published')? 'badge-inverse' : 'badge-primary'; ?>"><?php echo $Posts->get_post_count($type, 'published'); ?></small></a>
 								<a href="posts.php?type=<?php echo $type; ?>&status=unpublished" class="btn btn-sm <?php echo (isset($_GET['status']) && $_GET['status'] == 'unpublished')? 'btn-primary active disabled' : ''; ?>">Unpublished <small class="badge <?php echo (isset($_GET['status']) && $_GET['status'] == 'unpublished')? 'badge-inverse' : 'badge-primary'; ?>"><?php echo $Posts->get_post_count($type, 'unpublished'); ?></small></a>
 								<a href="posts.php?type=<?php echo $type; ?>&status=deleted" class="btn btn-sm <?php echo (isset($_GET['status']) && $_GET['status'] == 'deleted')? 'btn-primary active disabled' : ''; ?>">Trash <small class="badge <?php echo (isset($_GET['status']) && $_GET['status'] == 'deleted')? 'badge-inverse' : 'badge-primary'; ?>"><?php echo $Posts->get_post_count($type, 'deleted'); ?></small></a>
-
 							</div>
-							<div class="col-sm-3 col-sm-offset-3">
+							<div class="col-sm-4">
+								<div class="form-horizontal">
+									<div class="form-group" style="margin-bottom:0">
+										<label class="col-sm-3 control-label"><?php __('Category'); ?>:</label>
+										<div class="col-sm-6">
+											<select name="category" class="form-control selectpicker">
+												<option value=""><?php __('All'); ?></option>
+												<?php 
+													$Categories = new categories();
+													$all_categories = $Categories->get_categories($type);
+
+													if ($all_categories) { 
+														foreach ($all_categories as $category) { 
+															$selected = (isset($_GET['category']) && $_GET['category'] == $category->category_id) ? 'selected' : '';
+															$selected = (isset($_POST['category']) && $_POST['category'] == $category->category_id) ? 'selected' : $selected;
+														?>
+														<option <?php echo $selected; ?> value="<?php echo $category->category_id; ?>"><?php echo $category->category_name; ?></option>
+												<?php }} ?>
+											</select>
+										</div>
+										<div class="col-sm-3">
+											<input type="submit" class="btn btn-default" value="<?php __('Filter'); ?>" name="filter">
+										</div>
+									</div>
+								</div>
+							</div><!-- // . col -->
+							<div class="col-sm-3">
 								<div class="form-horizontal">
 									<div class="form-group" style="margin-bottom:0">
 										<label class="col-sm-3 control-label"><?php __('Actions'); ?>:</label>
 										<div class="col-sm-5">
 											<select name="action" class="form-control selectpicker">
-												<option value="">None</option>
-												<option value="published">Publish</option>
-												<option value="unpublished">Un Publish</option>
-												<option value="deleted">Delete</option>
+												<option value=""><?php __('None'); ?></option>
+												<option value="published"><?php __('Publish'); ?></option>
+												<option value="unpublished"><?php __('Un Publish'); ?></option>
+												<option value="deleted"><?php __('Delete'); ?></option>
 											</select>
 										</div>
 										<div class="col-sm-4">
-											<input type="submit" class="btn btn-default" value="Apply" name="submit">
+											<input type="submit"  onClick="return confirm('<?php echo 'Do you realy want to apply this action?' ?>');" class="btn btn-default" value="<?php __('Apply'); ?>" name="submit">
 										</div>
 									</div>
 								</div>
@@ -161,10 +197,10 @@ include 'include/header.php';
 						<table id="SampleDT" class="datatable table table-bordered table-striped table-hover tc-table table-primary footable">
 							<thead>
 								<tr>
-									<th class="col-small center"><label><input type="checkbox" class="tc"><span class="labels"></span></label></th>
+									<th class="col-small hidden-xs center"><label><input type="checkbox" class="tc"><span class="labels"></span></label></th>
 									<th><?php __('Name') ?></th>
-									<th class="hidden-xs col-large center"><i class="fa fa-user" title="Author"></i></th>
-									<th class="hidden-xs col-medium center"><i class="fa fa-comments" title="Post Comments"></i></th>
+									<th class="hidden-xs" style="width:160px; text-align: center;"><i class="fa fa-user" title="Author"></i></th>
+									<th class="hidden-xs" style="width:80px; text-align: center;"><i class="fa fa-comments" title="Post Comments"></i></th>
 									<th class="hidden-xs col-medium center"><i class="fa fa-calendar" title="Date"></i></th>
 									<th class="center col-medium"><?php __('Actions') ?></th>
 								</tr>
@@ -181,9 +217,9 @@ include 'include/header.php';
 											}
 											?>
 												<tr>
-													<td class="col-small center"><label><input type="checkbox" class="tc" name='posts[]' value="<?php echo $post->ID; ?>"><span class="labels"></span></label></td>
+													<td class="col-small hidden-xs center"><label><input type="checkbox" class="tc" name='posts[]' value="<?php echo $post->ID; ?>"><span class="labels"></span></label></td>
 													<td><?php echo $indent.' '.$post->title ?></td>
-													<td>
+													<td class="hidden-xs">
 														<?php 
 															// Author
 															$Users = new users();
@@ -191,8 +227,8 @@ include 'include/header.php';
 															echo $profile->user_display_name;
 														?>
 													</td>
-													<td class="col-medium center"><small class="badge center"><?php echo $post->comments; ?></small></td>
-													<td class="col-medium center"><?php echo $Users->_date('d-m-Y', $post->cts); ?></td>
+													<td class="hidden-xs" style="width:80px; text-align: center;"><small class="badge center"><?php echo $post->comments; ?></small></td>
+													<td class="hidden-xs col-medium center"><?php echo $Users->_date('d-m-Y', $post->cts); ?></td>
 													<td class="center col-medium">
 														<div class="btn-group btn-group-sm">
 															<a href="post_create.php?type=<?php echo $type; ?>&ID=<?php echo $post->ID; ?>" class="btn btn-inverse"><i class="fa fa-pencil icon-only"></i></a>
