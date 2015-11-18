42d18eaea02d02a4af566303cb40988502c4f383
Muhammad Hanif <thehanif@msn.com>
2015-11-18 22:51:52 +0500

    Language
    Playing with urdu language

--- admin/languages/ur-PK/language.php
+++ admin/languages/ur-PK/language.php
@@ -5,13 +5,17 @@
  * Direction : RTL
  */
 
-$language['Title'] = 'Title';
-$language['Dashboard'] = 'Dashboard';
-$language['Login Form'] = 'Login Form';
+$language['Title'] = 'عنوان';
+$language['Dashboard'] = 'ڈیش بورڈ';
+$language['Login Form'] = 'لاگ ان فارم';
+$language['Login'] = 'لاگ ان';
 
-$language['Incorrect Username or Password, please try again.'] = 'Incorrect Username or Password, please try again.';
-$language['Click here to reset it.'] = 'Click here to reset it.';
-$language['Forgot your password?'] = 'Forgot your password?';
-$language['Password'] = 'Password';
-$language['Username'] = 'Username';
-$language['Settings'] = 'Kuj nahe';
+$language['Incorrect Username or Password, please try again.'] = 'غلط صارف کا نام یا پاس ورڈ، دوبارہ کوشش کریں.';
+$language['Click here to reset it.'] = 'اسے ری سیٹ کرنے کے لئے یہاں کلک کریں.';
+$language['Forgot your password?'] = 'آپ اپنا پاس ورڈ بھول گئے ہیں؟';
+$language['Password'] = 'پاس ورڈ';
+$language['Username'] = 'صارف کا نام';
+$language['Settings'] = 'ترتیبات';
+$language['Language'] = 'زبان';
+$language['Submit'] = 'محفوظ کریں';
+$language['Change admin language.'] = 'منتظم زبان تبدیل کریں.';
