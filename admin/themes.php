abd7ae5b11086d6bf85f34445269568416f1c294
Muhammad Hanif <thehanif@msn.com>
2016-01-26 21:48:04 +0500

    Themes
    Added load theme functionality

--- admin/themes.php
+++ admin/themes.php
@@ -2,11 +2,29 @@
 // Initialization
 include_once 'include/init.php';
 
+$themes = new themes();
+
+$is_allowed = is_allowed(NULL, array('Site'=>array('manage-themes')));
+
+// Settings object
+$Settings = new settings();
+
+// Proccess submited data
+if (isset($_GET['loadtheme']) && $is_allowed) {
+
+	if($themes->load_theme($_GET['loadtheme']) > 0){
+		register_admin_message('Success', 'Theme loaded successfully.', 'success');
+	}	
+}
+
 // Page title
 $admin_title = 'Themes';
 
-$themes = new themes();
+// List all installed theme from theme dir
 $theme_list = $themes->get_theme_list();
+
+// Current site theme
+$current_theme = $themes->get_theme();
 
 // Header file
 include 'include/header.php';
@@ -43,16 +61,16 @@ include 'include/header.php';
 					foreach ($theme_list as $key => $info) {
 						?>
 							<div class="col-md-4 col-sm-3 col-xs-12 col-lg-2">
-								<div class="panel panel-default">
+								<div class="panel <?php echo ($current_theme != $key)? 'panel-default' : 'panel-info'; ?>">
 									<div class="panel-body">
 										<img src="<?php echo $info['preview']; ?>" alt="<?php echo $info['name']; ?>" class="img-responsive">
 										<ul class="list-group">
-											<li class="list-group-item"><strong>Name:</strong> <?php echo $info['name']; ?></li>
-											<li class="list-group-item"><strong>Author:</strong> <?php echo $info['author']; ?></li>
-											<li class="list-group-item"><strong>Info:</strong> <?php echo $info['description']; ?></li>
+											<li class="list-group-item"><strong><?php __('Name:'); ?></strong> <?php echo $info['name']; ?></li>
+											<li class="list-group-item"><strong><?php __('Author:'); ?></strong> <?php echo $info['author']; ?></li>
+											<li class="list-group-item"><strong><?php __('Info:'); ?></strong> <?php echo $info['description']; ?></li>
 										</ul>
-										<a href="#" class="btn btn-info" title="Apply theme">Apply theme</a>
-										<a href="#" class="btn btn-danger" title="Delete theme"><i class="fa fa-trash-o"></i></a>
+										<a href="themes.php?loadtheme=<?php echo $key; ?>" class="btn btn-info <?php echo ($current_theme != $key)? '' : 'disabled'; ?>" title="<?php __('Apply theme'); ?>"><?php __('Apply theme'); ?></a>
+										<a href="#" class="btn btn-danger <?php echo ($current_theme != $key)? '' : 'disabled'; ?>" title="<?php __('Delete theme'); ?>"><i class="fa fa-trash-o"></i> </a>
 									</div>
 								</div>
 							</div>
