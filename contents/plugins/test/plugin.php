fe8a6f2e7d86a2d2ee22b3ead978dd42afcf1da7
Muhammad Hanif <thehanif@msn.com>
2016-03-17 01:07:13 +0500

    Test
    Testing capabilities for plugin

--- contents/plugins/test/plugin.php
+++ contents/plugins/test/plugin.php
@@ -7,3 +7,16 @@ Description: Test Description of the theme
 
 URL: http://google.com
 */
+
+
+// print_f($Users->get_capabilities_groups());
+
+// Plugins
+$HANIF = array(
+		'name' 				=> 	'HANIF'
+		,'title' 			=>	'HANIF'
+		, 'icon' 			=>	'desktop'
+		, 'file'			=>	'plugins.php'
+		, 'capability'		=> 	array('Plugins'=>array('manage-plugins'))
+	);
+add_admin_menu_item($HANIF);
